d6854091d87657cde2411b2b0efc7367249aeda7
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-06-03 13:08:10 -0500

    adds homepage layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,8 +3,17 @@ import React from "react";
 function HomePage() {
   return (
     <div className="home-content" id="home">
-      <section>
-        <h1>CONTENT GOES HERE</h1>
+      <section className="home-info">
+        <h1>Welcome to Melody Architect</h1>
+        <p>an app designed with musicians in mind</p>
+        <button
+          id="start-btn"
+          type="submit"
+          name="get started"
+          className="start-btn"
+        >
+          Let's Get Started
+        </button>
       </section>
     </div>
   );
